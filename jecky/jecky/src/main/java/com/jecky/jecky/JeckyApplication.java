6e6d06cbbe347b2e0b042d03ca522f6f4d32661b
jecky <jkyhdy00711@gmail.com>
2020-07-19 23:05:25 +0700

    menambahkan crud

---
+++ jecky/jecky/src/main/java/com/jecky/jecky/JeckyApplication.java
@@ -0,0 +1,13 @@
+package com.jecky.jecky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeckyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeckyApplication.class, args);
+	}
+
+}
---
+++ jecky/jecky/src/test/java/com/jecky/jecky/JeckyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecky.jecky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeckyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
