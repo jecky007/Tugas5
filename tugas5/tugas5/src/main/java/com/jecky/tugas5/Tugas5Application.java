b93581fba9fc3b8f6690a471b8c012477ed11db7
jecky <jkyhdy00711@gmail.com>
2020-07-25 18:03:54 +0700

    tugas5 mongodb

---
+++ tugas5/tugas5/src/main/java/com/jecky/tugas5/Tugas5Application.java
@@ -0,0 +1,13 @@
+package com.jecky.tugas5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas5Application.class, args);
+	}
+
+}
---
+++ tugas5/tugas5/src/test/java/com/jecky/tugas5/Tugas5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecky.tugas5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
